0ae7de572de68f2ecc1f64f37aaa476eb3fa301e
Abdelrahman Hany <98924552+AbdelrhmanAbuelnile@users.noreply.github.com>
2023-09-17 01:49:00 +0300

    let's go

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        social: {
+          100: '#3b5998', //Facebook hex color
+          200: '#0e76a8', //Linkedin hex color
+          300: '#00acee', //Twitter hex color
+          400: '#8a3ab9', //Instagram hex color
+          500: '#171515', //Github hex color
+          600: '#facc15',
+          1: '#FEDA77',
+          2: '#DD2A7B',
+          3: '#8134AF',
+          4: '#fcde00',
+          5: '#03C03C',
+        },
+        track: {
+          1: '#1144ec', //solar
+          2: '#3bcbf1', //graphic
+          3: '#cc3900', //robotics
+          4: '#6c3bf1', //web
+          5: '#76ba6e', //arch
+        },
+      },
+    },
+    screens: {
+      'xs': '350px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  plugins: [],
+}
+
