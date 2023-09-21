0997653ee21ed2e1d0f382cbcf20c60485a22507
NightMare489 <123az.az.Az.yaser@gmail.com>
2023-09-21 18:26:25 +0300

    editing routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,28 @@ import { CodeStorm } from "./pages/CodeStorm"
 import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import CodeStormForm from "./pages/CodeStormForm"
 import {Route, Link, Routes} from 'react-router-dom';
+import { useNavigate } from "react-router";
 
 function App() {
 
 
-  
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />
-    },
-    {
-      path: '/codestorm',
-      element: <CodeStorm />,
-    },
-    {
-      path: '/form',
-      element: <CodeStormForm />
-    }
-  ])
+  if(window.location.pathname === "/codestorm") {
+    const navigae = useNavigate();
+    navigae("/codestorm");
+  }
+  if(window.location.pathname === "/form") {
+    const navigae = useNavigate();
+    navigae("/form");
+  }
+
   return (
-    <BrowserRouter>
+
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/codestorm" element={<CodeStorm />} />
           <Route path="/form" element={<CodeStormForm />} />
         </Routes>
-       </BrowserRouter>
+
   )
 }
 
