239f28dd6a3c7a1fcec7f5c1bd0f17d57de9892b
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-02-10 01:13:45 +0200

    THE SERVER SHALL LIVE AGAIN
    - with disability -

--- api/routes/api.routes.js
+++ api/routes/api.routes.js
@@ -1,96 +1,90 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 const {
-    visitorRegister,
-    exportVisitors
-} = require('../controllers/visitor.controller')
+  visitorRegister,
+  exportVisitors,
+} = require("../controllers/visitor.controller");
 
 const {
-    applicantRegister,
-    exportApplicants
-} = require('../controllers/applicant.controller')
+  applicantRegister,
+  exportApplicants,
+} = require("../controllers/applicant.controller");
 
 const {
-    techtipRegister,
-    exportTechtip
-} = require('../controllers/techtip.controller')
+  techtipRegister,
+  exportTechtip,
+} = require("../controllers/techtip.controller");
 
 const {
-    contactReg,
-    exportContact
-} = require('../controllers/contact.controller')
+  contactReg,
+  exportContact,
+} = require("../controllers/contact.controller");
 
 const {
-    validateCertificate,
-    ExtractPDF,
-    importCertificates,
-    sendMail
-} = require('../controllers/certificate.controller')
+  validateCertificate,
+  ExtractPDF,
+  importCertificates,
+  sendMail,
+} = require("../controllers/certificate.controller");
 
 const {
-    beincampRegister,
-    exportBeincamp,
-    exportBeinTicket,
-} = require('../controllers/beincamp.controller')
+  beincampRegister,
+  exportBeincamp,
+  exportBeinTicket,
+} = require("../controllers/beincamp.controller");
 
 const {
-    beincamp6Register,
-    beincamp6Validate,
-    beincamp6recordAttendance
-} = require('../controllers/beincamp6.controller')
+  beincamp6Register,
+  beincamp6Validate,
+  beincamp6recordAttendance,
+} = require("../controllers/beincamp6.controller");
 
-const{
-    sendcodestormuser,
-    exportCodeStorm,
-    exportCodeStormTicket,
-    VerifyCodeStorm,
-} = require('../controllers/codestorm.controller')
+const {
+  sendcodestormuser,
+  exportCodeStorm,
+  exportCodeStormTicket,
+  VerifyCodeStorm,
+} = require("../controllers/codestorm.controller");
 
 //router.post('/api/visitor', visitorRegister)
 
-router.get('/api/export-visitors', exportVisitors)
-
+router.get("/api/export-visitors", exportVisitors);
 
 //router.post('/api/applicant', applicantRegister)
 
-router.get('/api/export-applicants', exportApplicants)
+router.get("/api/export-applicants", exportApplicants);
 
+router.post("/api/techtip", techtipRegister);
+router.get("/api/export-techtip", exportTechtip);
 
-router.post('/api/techtip', techtipRegister)
-router.get('/api/export-techtip', exportTechtip)
+router.post("/api/contact", contactReg);
+router.get("/api/export-contact", exportContact);
 
-router.post('/api/contact', contactReg)
-router.get('/api/export-contact', exportContact)
+router.post("/api/validate-certificate", validateCertificate);
 
-router.post('/api/validate-certificate',validateCertificate)
+router.post("/api/extract-pdf", ExtractPDF);
 
-router.post('/api/extract-pdf', ExtractPDF)
+router.post("/api/import-certificates", importCertificates);
 
-router.post('/api/import-certificates',importCertificates)
+router.post("/api/bein-camp", beincampRegister);
+router.get("/api/export-beincamp", exportBeincamp);
 
-router.post('/api/bein-camp',beincampRegister)
-router.get('/api/export-beincamp', exportBeincamp)
-
-
-router.post('/api/export-beinticket', exportBeinTicket)
-
+router.post("/api/export-beinticket", exportBeinTicket);
 
 // router.post('/api/send-users',sendcodestormuser)
-router.get('/api/export-codestorm', exportCodeStorm)
+router.get("/api/export-codestorm", exportCodeStorm);
 
-router.post('/api/export-codestormticket', exportCodeStormTicket)
+router.post("/api/export-codestormticket", exportCodeStormTicket);
 
-router.post('/api/verify-codestorm', VerifyCodeStorm)
+router.post("/api/verify-codestorm", VerifyCodeStorm);
 
-router.post('/api/codestorm-email', sendMail)
+router.post("/api/codestorm-email", sendMail);
 
-router.post('/api/bein6-register', beincamp6Register)
+// router.post("/api/bein6-register", beincamp6Register);
 
 router.post('/api/bein6-validate', beincamp6Validate)
 
-router.post('/api/bein6-record-attendance', beincamp6recordAttendance)
+router.post("/api/bein6-record-attendance", beincamp6recordAttendance);
 
-
-
-module.exports = router
+module.exports = router;
