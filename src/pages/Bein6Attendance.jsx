c32b600a7d72df091eec2101fc867ce6e4b0d275
NightMare489 <56297925+NightMare489@users.noreply.github.com>
2024-02-11 11:55:24 +0200

    Update Bein6Attendance.jsx

--- src/pages/Bein6Attendance.jsx
+++ src/pages/Bein6Attendance.jsx
@@ -2,42 +2,47 @@ import { useState, useEffect } from "react";
 import { QrReader } from "react-qr-reader";
 import { RecordAttendanceBein6 } from "../../services/register.service";
 import Info from "./Info";
+import {
+  Box,
+
+} from "@mui/material";
 
 export function Bein6Attendance() {
   const [open, setOpen] = useState(false);
   const [severity, setSeverity] = useState("success");
+  const [data, setData] = useState(null);
+  const [response, setResponse] = useState('');
 
-  const [stamp, setStamp] = useState(0);
-  const [ticketID, setTicketID] = useState("");
-  const [response, setResponse] = useState("");
+  useEffect(() => {
+    if (!!data) {           
+        RecordAttendanceBein6({TicketID:data})
+        .then((res) => {
+          setOpen(false);
+          setResponse(res?.message);
+          setSeverity("success");
+          setOpen(true);
+          setTimeout(() => {
+            setData(null);
+          }
+          , 3000);
+        })
+        .catch((err) => {
+          setOpen(false);
+          setResponse(err?.response?.data?.message || "An error occurred");
+          setSeverity("error");
+          setOpen(true);
 
-  const handelAttendance = (result, ticketID) => {
-    console.log(ticketID);
-    if (result?.text === null) return;
-    // if (result?.text === ticketID) console.log("rep");
+          setTimeout(() => {
+            setData(null);
+          }
+          , 3000);
 
-    setTicketID("testing");
-    setStamp(result?.timestamp);
-
-    RecordAttendanceBein6({ TicketID: result?.text })
-      .then((res) => {
-        setOpen(false);
-
-        setResponse(res.message);
-        setSeverity("success");
-        setOpen(true);
-      })
-      .catch((err) => {
-        setOpen(false);
-
-        setResponse(err.response.data.message);
-        setSeverity("error");
-        setOpen(true);
-      });
-  };
+        });
+    }    
+  }, [data]);
 
   return (
-    <>
+    <Box>
       <QrReader
         constraints={{
           facingMode: "environment",
@@ -45,14 +50,12 @@ export function Bein6Attendance() {
         key="environment"
         onResult={(result, error) => {
           if (!!result) {
-            console.log(result);
-            handelAttendance(result, ticketID);
+            setData(result?.text);
           }
         }}
-        style={{ width: "100%" }}
+        style={{
+           width: "100%"}}
       />
-      <p>{response}</p>
-      {/* <h1>ds</h1> */}
 
       <Info
         response={response}
@@ -60,6 +63,6 @@ export function Bein6Attendance() {
         setOpen={setOpen}
         severity={severity}
       />
-    </>
+    </Box>
   );
 }
