79c2be781e5a9a9c83d57e0a9d7b464c0c5086ec
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-02-11 14:12:59 +0200

    Update API base URL for development

--- services/api.js
+++ services/api.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 // const baseURL = process.env.API_DOMAIN + '/api/';
 
-const baseURL = 'http://localhost:8081/api/'; //for development
+// const baseURL = 'http://localhost:8081/api/'; //for development
 // const baseURL = "https://ieee.aswu.edu.eg/api/"; //for deployment
+const baseURL = "http://10.171.240.166:8081/api/"; //for development
 
 export const api = axios.create({
   baseURL,
